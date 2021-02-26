d0070ba02ed412f6095354a23a5555f5c8d018c3
kr-g <k.r.goger@gmail.com>
2021-02-26 08:09:17 +0100

    changed dependency

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
     ),
     keywords="double linked list file storage memory management pyheapfile",
     install_requires=[
-        "pyheapfile>=0.0.2a",
+        "pyheapfile>=0.0.2",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
