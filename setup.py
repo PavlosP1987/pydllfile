04ba0fdcbf58fc59bebef29ce39591f2af6f8e09
kr-g <k.r.goger@gmail.com>
2021-04-12 21:23:57 +0200

    version v0.0.4

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
     ),
     keywords="double linked list file storage memory management pyheapfile",
     install_requires=[
-        "pyheapfile>=0.0.4",
+        "pyheapfile",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
